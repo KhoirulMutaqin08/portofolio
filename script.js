d6b0c9659d116577cba13e1f1d4025d7401a1019
khoirul mutaqin <100250229+KhoirulMutaqin08@users.noreply.github.com>
2026-04-01 10:03:01 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const text = [
+  "Web Developer",
+  "Frontend Developer",
+  "UI Designer"
+];
+
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+  current = text[i];
+
+  if (isDeleting) {
+    document.getElementById("typing").textContent =
+      current.substring(0, j--);
+  } else {
+    document.getElementById("typing").textContent =
+      current.substring(0, j++);
+  }
+
+  if (!isDeleting && j === current.length) {
+    isDeleting = true;
+    setTimeout(type, 1000);
+    return;
+  }
+
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % text.length;
+  }
+
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
